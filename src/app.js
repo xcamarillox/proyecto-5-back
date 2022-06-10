a7a7b3a955253571b3a31e298fd10384a4fb4e44
CARLOS CAMARILLO <carlos.adrian.camarillo@gmail.com>
2022-06-10 13:14:00 -0500

    Refactoring on app.js.

--- src/app.js
+++ src/app.js
@@ -2,92 +2,61 @@ const express = require('express');
 const { getAPIdata, ACTIONS_LIST } = require('./api-helpers');
 const app = express();
 
-app.get('', function(req, res) {
-    res.send('Hello World');
-})
-
-const handleFullfilledRequest = (res, isRequestSuccesful, response) => {
+const handleRequest = async(res, action) => {
+    let response;
+    let isRequestSuccesful = false;
+    try {
+        response = await getAPIdata(action);
+        isRequestSuccesful = true;
+    } catch (error) { response = error; }
     if (isRequestSuccesful) res.send(response.data)
     else res.send("Error")
 }
 
 app.get('/trending/movie/week', async(req, res) => {
-    let isRequestSuccesful = false;
-    let response;
-    try {
-        response = await getAPIdata({
-            type: ACTIONS_LIST.SEARCH_TRENDS
-        });
-        isRequestSuccesful = true;
-    } catch (error) { response = error; }
-    handleFullfilledRequest(res, isRequestSuccesful, response)
+    await handleRequest(res, {
+        type: ACTIONS_LIST.SEARCH_TRENDS
+    })
 })
 
 app.get('/search/:searchParam', async(req, res) => {
-    let isRequestSuccesful = false;
-    let response;
-    try {
-        response = await getAPIdata({
-            type: req.params.searchParam == 'movie' ? ACTIONS_LIST.SEARCH_FOR_MOVIES : ACTIONS_LIST.SEARCH_FOR_ARTIST,
-            searchedMovie: req.query.query,
-            searchedArtist: req.query.query
-        });
-        isRequestSuccesful = true;
-    } catch (error) { response = error; }
-    handleFullfilledRequest(res, isRequestSuccesful, response)
+    await handleRequest(res, {
+        type: req.params.searchParam == 'movie' ?
+            ACTIONS_LIST.SEARCH_FOR_MOVIES : ACTIONS_LIST.SEARCH_FOR_ARTIST,
+        searchedMovie: req.query.query,
+        searchedArtist: req.query.query
+    });
 })
 
 app.get('/movie/:movieId', async(req, res) => {
-    let isRequestSuccesful = false;
-    let response;
-    try {
-        response = await getAPIdata({
-            type: ACTIONS_LIST.SEARCH_FOR_MOVIE_DETAILS,
-            movieId: req.params.movieId
-        });
-        isRequestSuccesful = true;
-    } catch (error) { response = error; }
-    handleFullfilledRequest(res, isRequestSuccesful, response)
+    await handleRequest(res, {
+        type: ACTIONS_LIST.SEARCH_FOR_MOVIE_DETAILS,
+        movieId: req.params.movieId
+    })
 })
 
 app.get('/movie/:movieId/credits', async(req, res) => {
-    let isRequestSuccesful = false;
-    let response;
-    try {
-        response = await getAPIdata({
-            type: ACTIONS_LIST.SEARCH_FOR_MOVIE_CREDITS,
-            movieId: req.params.movieId
-        });
-        isRequestSuccesful = true;
-    } catch (error) { response = error; }
-    handleFullfilledRequest(res, isRequestSuccesful, response)
+    await handleRequest(res, {
+        type: ACTIONS_LIST.SEARCH_FOR_MOVIE_CREDITS,
+        movieId: req.params.movieId
+    })
 })
 
 app.get('/person/:artistId', async(req, res) => {
-    let isRequestSuccesful = false;
-    let response;
-    try {
-        response = await getAPIdata({
-            type: ACTIONS_LIST.GET_ARTIST_DATA,
-            artistId: req.params.artistId
-        });
-        isRequestSuccesful = true;
-    } catch (error) { response = error; }
-    handleFullfilledRequest(res, isRequestSuccesful, response)
+    await handleRequest(res, {
+        type: ACTIONS_LIST.GET_ARTIST_DATA,
+        artistId: req.params.artistId
+    })
 })
 
 app.get('/person/:artistId/movie_credits', async(req, res) => {
-    let isRequestSuccesful = false;
-    let response;
-    try {
-        response = await getAPIdata({
-            type: ACTIONS_LIST.GET_FEATURING_MOVIES,
-            artistId: req.params.artistId
-        });
-        isRequestSuccesful = true;
-    } catch (error) { response = error; }
-    handleFullfilledRequest(res, isRequestSuccesful, response)
+    await handleRequest(res, {
+        type: ACTIONS_LIST.GET_FEATURING_MOVIES,
+        artistId: req.params.artistId
+    })
 })
+
+app.get('*', (req, res) => { res.send("URL not a valid as a Endpoint") })
 
 let server = app.listen(3000, function() {
     let host = server.address().address
